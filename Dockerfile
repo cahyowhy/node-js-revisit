af0a89ddbd89de6fef6f97693052bf242994c52c
cahyo wibowo <cahyowhy01@gmail.com>
2021-04-04 12:41:31 +0700

    add health-check routes

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,12 @@ WORKDIR /usr/src/app
 COPY ["package.json", "package-lock.json*", "npm-shrinkwrap.json*", "./"]
 COPY prod.env ./.env
 
+# npm install --production to prevent install dev dependencies
+
+# mv node_modules ../ https://stackoverflow.com/questions/54198947/why-move-node-modules-to-parent-directory
+# leverage the advantage of caching mechanism on docker.
+# So in our case, it'll only run npm install… if the previous layer had changes, say an updated package.json. 
+# If not, then it won't execute the npm install command, even if, say, index.js might have changed.
 RUN npm install --production --silent && mv node_modules ../
 COPY . .
 EXPOSE 3000
--- src/index.ts
+++ src/index.ts
@@ -8,19 +8,79 @@ import ajv from './config/ajv';
 
 require('dotenv').config();
 
+const healthCheck = require('healthchecks-api');
+const path = require('path');
+
 const app = express();
 
-DatabaseConnection.setup().then(() => {
+DatabaseConnection.setup().then(async () => {
+  app.use(express.json());
+  app.use(cookieParser());
+  app.use(express.urlencoded({ extended: false }));
+
+  // custom adapter so won't errror on windows
+  const adapter = async (service: any, server: any, route: any) => {
+    const url: string = path.join('/status', route.path).replace(/\\/g, '/');
+
+    // 1. Expose given route in the `http` server application, here an example for the `Express.js`:
+    // eslint-disable-next-line implicit-arrow-linebreak
+    return server.get(url, async (req: any, res: any, next: any) => {
+      try {
+        // 2. Combine the `Express.js` route parameters:
+        const params = { ...req.params, ...req.query, ...req.headers };
+        // 3. Call given `route.handler` passing combined parameters and given service descriptor:
+        const result = await route.handler(params, service);
+        // 4. Decorate the `Express.js` response:
+        res.status(result.status);
+        res.set('Content-Type', result.contentType);
+        res.set(result.headers);
+        // 5. Return the response body according to given `contentType`:
+        switch (result.contentType) {
+          case 'application/json':
+            res.json(result.body);
+            break;
+          default:
+            res.send(result.body);
+        }
+      } catch (err) {
+        // 6. Deal with the Error according to `Express.js` framework rules.
+        next(err);
+      }
+    });
+  };
+
+  // see https://hootsuite.github.io/health-checks-api/
+  await healthCheck(app, {
+    adapter,
+    service: {
+      config: {
+        name: 'node-js-revisit',
+        description: 'Demo app for studying about typescript',
+        checks: [
+          // this need python
+          /* {
+            check: 'self',
+            memwatch: 'memwatch-next',
+          }, */
+          {
+            name: 'mongo',
+            url: process.env.DB_URL,
+            type: 'external',
+            interval: 3000,
+            check: 'mongo',
+          },
+        ],
+      },
+    },
+  });
+
+  app.use('/', router(logger, ajv));
+  app.use(errorHandler);
+
   app.listen(process.env.PORT, () => {
     // eslint-disable-next-line no-console
     console.log('app are running on port : 3000');
   });
-
-  app.use(express.json());
-  app.use(cookieParser());
-  app.use(express.urlencoded({ extended: false }));
-  app.use('/', router(logger, ajv));
-  app.use(errorHandler);
 }).catch((e) => {
   logger.log({ level: 'error', message: e.toString() });
   process.exit();
